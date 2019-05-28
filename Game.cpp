f39226f5681e9d8588d42fdf06a5e78691638f87
Gage13 <dplak1@ocdsb.ca>
2019-05-28 10:01:31 -0400

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,1 +1,23 @@
+#include <SFML/Graphics.hpp>
 
+class Game
+{
+public:
+  Game();
+  void run();
+private:
+  void processEvents();
+  void update();
+  void render();
+private:
+  sf::RenderWindow mWindow;
+  sf::CircleShape mCircel;
+};
+
+int main()
+{
+  Game game;
+  game.run();
+  
+  return 0;
+}
